64d78cca8e62b3861ba9509f557c19278cb25e34
Samuel Nogueira <samuel.nogueira716@gmail.com>
2022-11-23 14:57:28 -0300

    Add files via upload

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import {Card} from '../Card/index.jsx'
 function App() {
   var tarefas = []
   var tarefaTeste1 = {id:"1",conteudo:"Tarefa 1"}
-  var tarefaTeste2 = {id:"2",conteudo:"Tarefa 2"}
+  var tarefaTeste2 = {id:"2",conteudo:"Tarefa 2aaaaaaaaaaa asdfadfsdf aaaaaaaaa fdsssdfdsf dsfsfsfsfsf dsffdssdfdfs asdddddddddddddddddddddddddddassss aaaaaaaaaas"}
   tarefas.push(tarefaTeste1)
   tarefas.push(tarefaTeste2)
   return (
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,32 +1,19 @@
 import './style.css'
-export const Card = ({tarefas}) =>{
-    return(
-        <div className='card'>
-            <div className='box_titulo'>
-                <p className='titulo'></p>
-                <button className='botao_excluir_card'>X</button>
-            </div>
-            <div className='conteudo_card'>
-                {tarefas.map(tarefa =>
-                    <div className='tarefa' key={tarefa.id}>
-                        <p>{tarefa.conteudo}</p>
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+export const Card = ({ tarefas }) => {
+  return (
+    <div className='card'>
+      <div className='box_titulo'>
+        <p className='titulo'>Tarefas Card 1</p>
+        <button className='botao_excluir_card'>Options</button>
+      </div>
+      <div className='conteudo_card'>
+        {tarefas.map(tarefa =>
+          <div className='tarefa' key={tarefa.id}>
+            <p>{tarefa.conteudo}</p>
+            <button className="botao_excluir_tarefa">X</button>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
-// var corpo = document.createElement("div");
-//     var div_titulo = document.createElement("div");
-//     var botao_excluir_card = document.createElement("button")
-//     var titulo_card = document.createElement("p");
-//     var botao_adicionar_tarefa = document.createElement("button")
-//     var conteudo_card = document.createElement("div")
-
-//     corpo.classList.add("card")
-//     div_titulo.classList.add("titulo_card")
-//     botao_adicionar_tarefa.classList.add("botao_adicionar_tarefa")
-//     botao_excluir_card.classList.add("botao_excluir_card")
-//     conteudo_card.classList.add("dropzone")
-//     botao_excluir_card.innerHTML = "X"
-//     botao_adicionar_tarefa.innerHTML = "+ tarefa"
