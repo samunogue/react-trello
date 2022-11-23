32f09eeae1dc03bd188246c5ae0719fccc876f4d
samunogue <samuel.nogueira716@gmail.com>
2022-11-23 11:54:55 -0300

    adicionando card

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 import { Header } from '../header/index.jsx';
+import {Card} from '../Card/index.jsx'
 function App() {
+  var tarefas = []
+  var tarefaTeste1 = {id:"1",conteudo:"Tarefa 1"}
+  var tarefaTeste2 = {id:"2",conteudo:"Tarefa 2"}
+  tarefas.push(tarefaTeste1)
+  tarefas.push(tarefaTeste2)
   return (
-    <Header />
+    <>
+      <Header />
+      <Card tarefas={tarefas}/>
+    </>
   )
 }
 
